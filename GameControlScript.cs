5a7ea5d3968f88f9410ec7ae3b7bd75b8d66bda1
ramius84 <hiddenworks11@gmail.com>
2017-07-18 22:54:55 +0200

    Initial commit (scripts)
    Added scripts for game control, player and objects behavior etc.

---
+++ GameControlScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControlScript : MonoBehaviour {
+
+	bool isRunning = true;
+	int playerScore = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddScore()
+	{
+		playerScore++;
+	}
+
+	public void PlayerDied()
+	{
+		isRunning = false;
+	}
+
+	void OnGUI()
+	{
+		if(isRunning == true)
+		{
+		GUI.Label (new Rect(5, 5, 100, 30), "Punkty: " + playerScore);
+	}
+	else
+	{
+		GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "Koniec gry. Liczba zdobytych punktow: " + playerScore);
+	}
+	}
+
+}
---
+++ MeteorSpawnScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeteorSpawnScript : MonoBehaviour {
+
+	float spawnThreshold = 100;
+	float spawnDecrement = .1f;
+
+	public GameObject meteor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Random.Range(0, spawnThreshold) <= 1)
+		{
+			Vector3 pos = transform.position;
+			Instantiate(meteor, new Vector3(pos.x + Random.Range(-6, 6), pos.y, pos.z), Quaternion.identity);
+			
+			spawnThreshold -= spawnDecrement;
+			if(spawnThreshold < 2)
+			{
+				spawnThreshold = 2;
+			}
+		}
+	
+	}
+}
